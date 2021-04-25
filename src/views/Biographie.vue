1ded3427c3ca3151017395723d682f071fbebb00
Dona <dona.denkey_dandjein@edu.devinci.fr>
2021-04-25 16:04:01 +0200

    ajout biographie

--- src/views/Biographie.vue
+++ src/views/Biographie.vue
@@ -1,43 +1,92 @@
 <template>
   <div class="bio">
-      <p>boy</p>
+    <div id="histoire">
+      <h1>Voici son histoire :</h1>
+    </div>
+    <div id="page-wrap">
+      <div class="part">
+        <img src="@/assets/img/hangbe.jpg" id="img1" alt="hangbè">
+        <p>Tasi Hangbè, fille de Houegbadja et sœur jumelle d'Akaba, avait un frère cadet nommé Dosu qui prit ensuite le nom d'Agadja qui est le nom traditionnel du frère cadet de jumeaux. Au 17ème siècle, à la mort de son père, Tasi Hangbè monte sur le trône et partage le pouvoir avec son jumeau, Akaba. Elle est réputée pour être « la reine sans pudeur », une libertine aux mœurs honteuses et dépravées car elle a toujours vécu dans la plus grande liberté. Celle-ci a décidé un jour de transformer le palais en salle d'amusement où toutes les beuveries sont permises.</p>
+      </div>
+
+      <div class="part">
+        <p>Plus tard, alors que le royaume est toujours en guerre contre ses ennemis jurés, « les Ouéménous », le roi Akaba est violemment emporté par la variole. 
+Pour ne pas nuire au moral des troupes, un conseiller royal a l'idée de travestir Tasi Hangbè et lui demande de remplacer son jumeau, le roi, sur le champ de bataille. 
+La reine accepte et plusieurs personnes font la remarque que dans les vêtements de son frère, elle lui ressemble comme deux gouttes d'eau.
+Voulant honorer la mémoire du roi, la reine Tasi décide de continuer la mission et mène ses guerriers jusqu'à la victoire. En tuant le chef des Ouéménous, elle met fin à la rivalité des deux peuples ennemis et acquiert une immense notoriété du jour au lendemain. C’est à partir de ce moment que Tasi devient alors la véritable reine du Dahomey
+</p>
+      </div>
+
+      <div class="part">
+        <img src="@/assets/img/Amazones_Bénin-2.jpg" alt="lesmino">
+        <p>Considérant les femmes tout aussi capables que les hommes, la reine fonde un régiment de combat exclusivement féminin, les Mino. Les Mino (désignée plus tard par les historiens européens sous le nom des Amazones du Dahomey), sont environs 4000 femmes entrainées à tuer sans sourciller. Choisies, dès l’adolescence, parmi les esclaves les plus robustes, elles sont formées à vénérer leur souveraine et à lui donner leur vie. Elles vivent dans les palais royaux pour prendre soin de leur reine et donc résignées à se marier.</p>
+      </div>
+
+      <div class="part">
+        <p>Malgré le fait que l’on n’ait rien à reprocher à la reine niveau stratégie, celle-ci est critiqué pour sa liberté sexuelle. Beaucoup de personnes, indignés d'être gouvernés par une reine débauchée, organisent en secret l'assassinat de son fils unique et le mettent en œuvre.</p>
+      </div>
+
+      <div class="part">
+        <p>Ce jour-là, Tasi Hangbè entre dans la salle des hommages, vêtue de ses pagnes en kita et elle s'installe sur le trône. On évoque sa bravoure au combat, sa grandeur de reine, ses guerrières robustes et sa douleur de mère ayant perdu son fils.
+Tasi, dépassée par les évènements et par les décisions prises, dénoue doucement ses pagnes et se retrouve nue devant une assistance bouche-bée. L’une de ses suiveuses, lui présente un vase rempli d'eau. La reine y glisse sa main et se lave les parties intimes pour montrer son mépris envers leurs choix. Elle hurle sa haine, sa douleur et maudit l'assemblée ainsi que tout le peuple du Dahomey, lui souhaitant un malheur sans fin. 
+Elle renonce ensuite à son titre de reine et sort de la pièce, à jamais.
+</p>
+      </div>
+
+      <div class="part">
+        <p>Le roi Agadja, son successeur, orgueilleux et misogyne, a largement effacé la reine Tasi Hangbè de l'histoire officielle du Dahomey. Malgré le fait que l’armée des femmes soldats ait disparu à la chute du Royaume du Dahomey lors de la conquête coloniale française, au XIXe siècle, quelques personnes se souviennent encore du règne de Tasi Hangbè et de ses guerrières, qui ont fait trembler plusieurs ennemis.</p>
+      </div>
+
+    </div>
   </div>
 </template>
 
 <style scoped>
+.bio{
+    margin: 0;
+    padding: 0;
+    background-color: black;
+    color: #eee;
+    scroll-snap-type: x mandatory;
+    overflow-x: scroll;
+    display: grid;
+    font-style: sans-serif;
+}
+
 /* width */
 ::-webkit-scrollbar {
-  width: 20px;
-  background: black;
+    width: 20px;
+    background: black;
 }
 
 /* Track */
 ::-webkit-scrollbar-track {
-  box-shadow: inset 0 0 5px;
-  border-radius: 10px;
-  background: black;
+    box-shadow: inset 0 0 5px;
+    border-radius: 10px;
+    background: black;
 }
 
 /* Handle */
 ::-webkit-scrollbar-thumb {
-  background: #bb9a73;
-  border-radius: 10px;
+    background: #bb9a73;
+    border-radius: 10px;
 }
 
 /* Handle on hover */
 ::-webkit-scrollbar-thumb:hover {
-  background: #a58763;
+    background: #a58763;
 }
 
-#page-wrap {
-  display: flex;
-  margin: 100px 40px 0;
-  height: 80vh;
-  text-align: justify;
-  position: relative;
+#page-wrap{
+    display: flex;
+    margin: 100px 40px 0;
+    height: 80vh;
+    text-align: justify;
+    position: relative;
+    
 }
 
-.part:nth-child(2n + 1) p {
+.part:nth-child(2n+1) p {
   width: 450px;
   text-align: center;
   position: absolute;
@@ -45,43 +94,42 @@
   margin-left: 25px;
 }
 
-.part {
-  width: 500px;
-  scroll-snap-align: start;
-  margin: 0 80px 0 10px;
-  text-align: center;
+  .part {
+    width: 500px;
+    scroll-snap-align: start;
+    margin:0 80px 0 10px;
+    text-align: center;
+  }
+  p {
+    font-size: 1.2em;
+    margin: 0 0 15px 0;
+    text-align: center;
+  }
+
+  #img1{
+    width: 240px;
+    height: 280px;
+    text-align: center;
 }
 
-p {
-  font-size: 1.2em;
-  margin: 0 0 15px 0;
-  text-align: center;
-}
+  .part img{
+      width: 370px;
+      height: 200px;
+      margin-bottom: 40px;
+      text-align: center;
+  }
 
-#img1 {
-  width: 240px;
-  height: 280px;
-  text-align: center;
-}
+  #histoire{
+    width: 100%;
+    margin-top: 20px;
+    text-align: center;
+    position: fixed;
+  }
 
-.part img {
-  width: 370px;
-  height: 200px;
-  margin-bottom: 40px;
-  text-align: center;
-}
-
-#histoire {
-  width: 100%;
-  margin-top: 20px;
-  text-align: center;
-  position: fixed;
-}
-
-#histoire h1 {
-  /* text-align: center; */
-  /* left:635px ; */
+  #histoire h1{
+    /* text-align: center; */
+     /* left:635px ; */
   font-size: 2.2em;
   letter-spacing: -1px;
-}
+  }
 </style>
--- src/views/Threejs.vue
+++ src/views/Threejs.vue
@@ -1,42 +1,204 @@
 <template>
   <div>
-    <h1>coucou</h1>
+      <div class="quizz">
+        <section class="bloc_questions">
+            <div id="quiz">
+                    <h1>Quiz</h1>
+                <div class="container">
+
+                    <h2 id="question"></h2>
+
+                    <h3 id="score"></h3>
+
+                    <div class="choices">
+                        <button id="guess0" class="btn">
+                            <p id="choice0"></p>
+                        </button>
+
+                        <button id="guess1" class="btn">
+                            <p id="choice1"></p>
+                        </button>
+
+                        <button id="guess2" class="btn">
+                            <p id="choice2"></p>
+                        </button>
+                    </div>
+
+                    <p id="progress"></p>
+
+
+
+                    <div id="reco">
+                        <a href="">Recommencer</a>
+                    </div>
+                </div>
+            </div>
+
+
+
+        </section>
+  </div>
   </div>
 </template>
 
-<script type="module">
-import * as THREE from "three";
-const scene = new THREE.Scene();
+// <script type="module">
+// import * as THREE from "three";
+// import * as OrbitControls from "three-orbitcontrols";
+// const scene = new THREE.Scene();
 
-const camera = new THREE.PerspectiveCamera(
-  75,
-  window.innerWidth / window.innerHeight,
-  0.1,
-  1000
-);
+// const camera = new THREE.PerspectiveCamera(
+//   75,
+//   window.innerWidth / window.innerHeight,
+//   0.1,
+//   1000
+// );
 
-const renderer = new THREE.WebGLRenderer();
-renderer.setSize(window.innerWidth, window.innerHeight);
-document.body.appendChild(renderer.domElement);
-const controls = new OrbitControls( camera, renderer.domElement );
-const geometry = new THREE.SphereGeometry(10, 10, 10);
-const material = new THREE.MeshNormalMaterial({ wireframe: true });
-const sphere = new THREE.Mesh(geometry, material);
-scene.add(sphere);
+// const renderer = new THREE.WebGLRenderer();
+// renderer.setSize(window.innerWidth, window.innerHeight);
+// document.body.appendChild(renderer.domElement);
+// const controls = new OrbitControls( camera, renderer.domElement );
+// const geometry = new THREE.SphereGeometry(10, 10, 10);
+// const material = new THREE.MeshNormalMaterial({ wireframe: true });
+// const sphere = new THREE.Mesh(geometry, material);
+// scene.add(sphere);
 
-camera.position.z = 5;
-controls.update();
-// camera.position.z = 70;
+// camera.position.z = 5;
+// controls.update();
+// // camera.position.z = 70;
 
-const animate = function () {
-  requestAnimationFrame(animate);
+// const animate = function () {
+//   requestAnimationFrame(animate);
 
-  sphere.rotation.x += 0.003;
-  sphere.rotation.y += 0.003;
-    controls.update();
-  renderer.render(scene, camera);
+//   sphere.rotation.x += 0.003;
+//   sphere.rotation.y += 0.003;
+//     controls.update();
+//   renderer.render(scene, camera);
   
-};
+// };
 
-animate();
+// animate();
 </script>
+
+
+<style scoped>
+
+.quizz{
+    z-index: 1000;
+}
+/* width */
+::-webkit-scrollbar {
+    width: 20px;
+    background: black;
+}
+
+/* Track */
+::-webkit-scrollbar-track {
+    box-shadow: inset 0 0 5px;
+    border-radius: 10px;
+    background: black;
+}
+
+/* Handle */
+::-webkit-scrollbar-thumb {
+    background: #bb9a73;
+    border-radius: 10px;
+}
+
+/* Handle on hover */
+::-webkit-scrollbar-thumb:hover {
+    background: #a58763;
+}
+
+
+article{
+    display: flex;
+    height: 722px;
+    justify-items: center;
+}
+
+
+.bloc_questions{
+    background-color: #000000;
+    /* width: 80%; */
+    display: grid;
+    justify-items: center;
+    text-align: center;
+    min-height: 100vh;
+    color: #dfe4ea;
+}
+
+#quiz h1 {
+    text-transform: uppercase;
+    font-size: 3rem;
+  }
+
+  #quiz h3 {
+    color: #db5c17;
+    font-size: 1.5rem;
+    margin-bottom: 60px;
+  }
+
+  body .container{
+      /* margin-top: 50px; */
+      align-items: center;
+      justify-items: center;
+      width: 950px;
+      height: 500px;
+  }
+
+  body .container .choices {
+    display: flex;
+    /* flex-wrap: wrap; */
+    margin: 3rem auto 3rem;
+    width: 800px;
+  }
+  body .container .choices button {
+    width: 400px;
+    height: 90px;
+    border-radius: 15px;
+    cursor: pointer;
+    border: none;
+    margin: 1rem;
+    outline: none;
+    background: #dfe4ea;
+    user-select: none;
+    font-weight: 600;
+  }
+  body .container .choices button:hover {
+    background: #db5c17;
+    transition: 0.5s;
+  }
+  body .container .choices button:active {
+    transform: scale(0.9);
+    transition: transform 0.1s;
+  }
+  body .container .choices button p {
+    font-size: 1.1rem;
+  }
+  body #progress {
+    font-size: 1.3rem;
+    color: #db5c17;
+    margin-bottom: 60px;
+  }
+
+
+  #reco a{
+    font-size: 1.3rem;
+    text-decoration: none;
+    padding: 20px;
+    border-radius: 15px;
+    height: 170px;
+    cursor: pointer;
+    border: none;
+    /* font-weight: 600;
+    margin: 2rem auto 0; */
+    color: #dfe4ea;
+    background-color: #db5c17;
+    
+  }
+  
+  #reco a:hover{
+    color: #db5c17;
+    background-color: #dfe4ea;
+  }
+</style>
